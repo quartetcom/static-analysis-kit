aa1da70260d1f55a8fdaaeb12efb7fd1942138f4
Naoki Ikeguchi (s6n.jp) <n-ikeguchi@quartetcom.co.jp>
2023-01-22 01:27:29 +0900

    ci: Add CI workflows

--- rector.php
+++ rector.php
@@ -8,6 +8,7 @@ use Rector\Config\RectorConfig;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths(array_map(fn (string $path) => __DIR__.$path, [
         '/src',
+        '/tests',
     ]));
 
     Config::use($rectorConfig);
