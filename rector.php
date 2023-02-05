3cfbdcbc65af2e60429f40daa864d132a345dd82
Naoki Ikeguchi (s6n.jp) <n-ikeguchi@quartetcom.co.jp>
2023-02-05 20:03:29 +0900

    fix: Move Config::use at the top

--- rector.php
+++ rector.php
@@ -6,10 +6,10 @@ use Quartetcom\StaticAnalysisKit\Rector\Config;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
+    Config::use($rectorConfig);
+
     $rectorConfig->paths(array_map(fn (string $path) => __DIR__ . $path, [
         '/src',
         '/tests',
     ]));
-
-    Config::use($rectorConfig);
 };
