831719a3fa1af194f8da5504c842d1b68e6ec514
Naoki Ikeguchi (s6n.jp) <n-ikeguchi@quartetcom.co.jp>
2023-03-03 14:46:59 +0900

    feat: Include paths configuration by default

--- ecs.php
+++ ecs.php
@@ -7,4 +7,9 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return function (ECSConfig $ecsConfig): void {
     Config::use($ecsConfig);
+
+    $ecsConfig->paths(array_map(fn (string $path) => __DIR__ . $path, [
+        '/src',
+        '/tests',
+    ]));
 };
